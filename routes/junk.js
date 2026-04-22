f525802c4a5889e11e599c7891b0dd970458b256
RasmusEriksson <rasmus.eriksson@elev.ga.ntig.se>
2026-04-22 11:09:34 +0200

    started making page for displaying singular junkpiece

--- routes/junk.js
+++ routes/junk.js
@@ -1,5 +1,7 @@
 import express from "express"
 import pool from "../config/db.js"
+import {param, validationResult } from "express-validator"
+import fs from "fs"
 
 const router = express.Router()
 
@@ -25,25 +27,61 @@ router.get("/", async (req, res, next) => {
 })
 
 
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", 
+    param("id").isInt().withMessage("ID must be a whole integer!"),
+    async (req, res, next) => {
     try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()){
+            return res.status(400).json({erorrs: errors.array()})
+        }
+
+
         var user = null
         if (req.session.authenticated) {
             user = req.session
         }
 
+        const pieceID = req.params.id
+
         var [rows] = await pool.query(` 
             SELECT * FROM piece
-            ORDER BY rating DESC
-            `)
+            WHERE piece.id = ?
+            `,[pieceID])
+        
+        if (rows.length > 0) {
+            const piece = rows[0]
 
-        res.render("junk/alljunk.njk",
-            { title: "Junkpieces", user: user, pieces: rows  }
-        )
+            var image_url = "/images/noImage.jpg"
+            var image_string = "/images/pieces/" + piece.name + ".jpg"
+            console.log("THIS IS THE URL: ", image_string)
+
+            if (fs.existsSync(image_string)){
+                image_url = image_string
+            }
+
+            return res.render("junk/junk.njk",
+                {user: user, piece: piece , img: image_url}
+            )
+        }
+        else {
+            throw new Error("Piece not found")
+        }
+
+        
     }
     catch(err) {
         next(err)
     }
 })
+
+function isValidUrl(string) {
+  try {
+    new URL(string);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
 
 export default router
